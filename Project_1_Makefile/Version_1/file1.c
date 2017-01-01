93baa4d6659d5be6a28ced241334bc4c3db6aeaf
Canhui WANG <1228923946@qq.com>
2017-01-01 16:18:27 +0800

    1

---
+++ Project_1_Makefile/Version_1/file1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "file1.h"
+
+void tool1(char *str){
+    printf("This is file1 print: %s",str);
+}
---
+++ Project_1_Makefile/Version_1/file1.h
@@ -0,0 +1,6 @@
+#ifndef file1_h
+#define file1_h
+
+void tool1(char *);
+
+#endif
