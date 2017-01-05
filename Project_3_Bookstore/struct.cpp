ebbc0644b675351133191f5c58c19b35a92cd1d4
Canhui WANG <1228923946@qq.com>
2017-01-05 10:23:58 +0800

    1

---
+++ Project_3_Bookstore/struct.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include "struct.h"
+
+using namespace std;
+
+int main(){
+
+	book bk1, bk2;
+	
+	bk1.isbn = "2342FD2FF";
+	bk1.numbers_sold = 12;
+	bk1.benefits = 142.12;
+
+	bk2.isbn = "5323FDGFG";
+	bk2.numbers_sold = 30;
+	bk2.benefits = 289;	
+			
+	cout << "book1.isbn: " << bk1.isbn << endl;
+	cout << "book1.numbers_sold: "<< bk1.numbers_sold << endl;
+	cout << "book1.benefits: " << bk1.benefits << endl << endl;	
+	
+	cout << "book2.isbn: " << bk2.isbn << endl;
+	cout << "book2.numbers_sold: " << bk2.numbers_sold << endl;
+	cout << "book2.benefits: " << bk2.benefits << endl << endl;
+
+	return 0;
+
+}
---
+++ Project_3_Bookstore/struct.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct book{
+
+	string isbn;
+	int numbers_sold;
+	double benefits;
+
+};
