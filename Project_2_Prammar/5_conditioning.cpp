0890850a14ebae982d4b2ed824c0e557580c65f8
Canhui WANG <1228923946@qq.com>
2017-01-08 10:56:15 +0800

    1

---
+++ Project_2_Prammar/5_conditioning.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int score;
+
+	cout <<"pls put in your score: ";
+	cin >> score;
+	
+	cout << ((score>=60)? "pass":"fail") << endl;
+
+	return 0;
+}
