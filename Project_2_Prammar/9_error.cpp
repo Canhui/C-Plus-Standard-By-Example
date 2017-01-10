c2a0d5a50fcbf1ed87ef09ed3c95e2386b072df0
Canhui WANG <1228923946@qq.com>
2017-01-11 02:59:37 +0800

    1

---
+++ Project_2_Prammar/9_error.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int a = -1;
+	
+	if(a==-1){
+		cerr << "error and exit" << endl;
+		return -1;
+	} else{
+		// do nothing
+	}	
+
+	return 0;
+
+}
