3188e30c220f6a96e5d0b4961f3fcbb92592cd7d
Canhui WANG <1228923946@qq.com>
2017-02-11 02:59:49 +0800

    1

---
+++ Project_2_Prammar/024_formats.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+#include <stdio.h>
+//using namespace std;
+
+int main(){
+	
+//	cout << "default bool values: " << 20 << endl;
+//	cout << "octal values: " << oct << 20 << endl;
+//	cout << "hex values: " << hex << 20 << endl;
+
+
+//	cout << "decimal values: " << dec << (signed)20 << endl;
+
+	printf("%d\n",(int)20); 
+
+	return 0;
+}
+
+
