9e5ddf1f4dea78590503258e6e717ebfed0a71d4
Canhui WANG <1228923946@qq.com>
2017-02-14 10:59:35 +0800

    1

---
+++ A_Second_Time/001_extern.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "001_extern.h"
+
+using namespace std;
+
+int main(){
+
+	int i = 1; // 声明和定义，并不会出现重复地声明和定义的问题
+	cout << i << endl;
+        
+	return 0;
+}
---
+++ A_Second_Time/001_extern.h
@@ -0,0 +1,4 @@
+#ifndef 001_extern_H
+#define 001_extern_H
+	extern int i; // 声明而没有定义
+#endif
