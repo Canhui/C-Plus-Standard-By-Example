51081e9f7a1a9073f198244d5fe25db86c691a95
Canhui WANG <1228923946@qq.com>
2017-02-16 02:29:29 +0800

    1

---
+++ A_Second_Time/003_string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+	string str1 = "Hello ";
+	string str2 = "World\n";
+	string str3 = str1 + str2;
+	cout << str3;
+
+	return 0;
+}
