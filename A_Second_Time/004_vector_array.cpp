ad2b31922ae37f7f9d18db1d19835d5b971d24ae
Canhui WANG <1228923946@qq.com>
2017-02-17 10:55:59 +0800

    11

---
+++ A_Second_Time/004_vector_array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(){
+
+	vector<int> array(10, 0);
+  
+        cin >>array[2] ;
+        cin >>array[5] ;
+        cin >>array[6] ;
+ 
+
+        for(int i=0; i<array.size(); i++)
+         	cout<<array[i]<<" " ;
+ 
+        return 0 ;
+}
