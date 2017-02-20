04b5d97dad7f51ad67210341db7471192b7ccc78
Canhui WANG <1228923946@qq.com>
2017-02-20 11:13:37 +0800

    1

---
+++ A_Second_Time/011_mul_array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+	int ma[2][2] = {{1,2},
+			{3,4}};
+
+	cout << ma[0][0]<<","<<ma[0][1]<<","<<ma[1][0]<<","<<ma[1][1] << endl;
+
+	return 0;
+}
