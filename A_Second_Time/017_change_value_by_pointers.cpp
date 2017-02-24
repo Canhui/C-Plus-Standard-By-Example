e0a4ccde18fb409f7126f3366857f6ee5b2a2b59
Canhui WANG <1228923946@qq.com>
2017-02-24 10:42:39 +0800

    1

---
+++ A_Second_Time/017_change_value_by_pointers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void reset(int *ip){
+	*ip = 0;
+}
+
+
+int main(){
+	int i = 42;
+	cout << "i = " << i << endl;
+	
+	reset(&i);
+	cout << "i = " << i << endl;
+	
+	return 0;
+}
